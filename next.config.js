1f91b2ae02f634ad5c016594b8e121a2e1e6fa99
Chikaysci <kjabellar@gmail.com>
2024-06-17 08:04:49 +0800

    added cache control

--- next.config.js
+++ next.config.js
@@ -1,7 +1,48 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=1, stale-while-revalidate=3600',
+          },
+        ],
+      },
+      {
+        source: '/partner',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=1, stale-while-revalidate=3600',
+          },
+        ],
+      },
+      {
+        source: '/about',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=1, stale-while-revalidate=3600',
+          },
+        ],
+      },
+      {
+        source: '/contact',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=1, stale-while-revalidate=3600',
+          },
+        ],
+      },
+    ];
+  },
   images: {
-    domains: ['cdn.vnoc.com','d2qcctj8epnr7y.cloudfront.net','vnoclogos.s3.amazonaws.com','contentbots.com', 'vnoclogos.s3-us-west-1.amazonaws.com', 'tools.contrib.com', 'contrib.com', 'www.contrib.com','vnoclogos.s3-us-west-1.amazonaws.com', 'vnoc.com', 's3.amazonaws.com'],
+    domains: ['vnoc.com','cdn.vnoc.com','d2qcctj8epnr7y.cloudfront.net','vnoclogos.s3.amazonaws.com','contentbots.com', 'vnoclogos.s3-us-west-1.amazonaws.com', 'tools.contrib.com', 'contrib.com', 'www.contrib.com','vnoclogos.s3-us-west-1.amazonaws.com', 'vnoc.com', 's3.amazonaws.com'],
   },
 }
 
