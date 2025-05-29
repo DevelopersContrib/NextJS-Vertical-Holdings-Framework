de25546b8a917806a4ec3f081d791062aa989fa5
julius-ph <71057113+juliusL03@users.noreply.github.com>
2025-05-29 15:17:14 +0800

    added name rss notification

--- components/includes/Learn.jsx
+++ components/includes/Learn.jsx
@@ -1,124 +1,125 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import Image from "next/image"
+import Image from "next/image";
 
+const Learn = ({ domain }) => {
+  const initialValues = {
+    domain: domain,
+    email: "",
+  };
 
-const Learn = ({domain}) => {
+  const initialErrors = {
+    validate: false,
+    emailError: "",
+  };
 
-    const initialValues = {
-        domain: domain,
-        email: ""
-    }
+  const [data, setData] = useState(initialValues);
+  const [errors, setErrors] = useState(initialErrors);
 
-    const initialErrors = {
-		validate:false,
-		emailError: "",
-	};
+  useEffect(() => {
+    const validateErrors = () => {
+      const dataErrors = {
+        emailError:
+          (data.email ? "" : "Email is required") ||
+          (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(data.email) ? "" : "Invalid Email"),
+      };
+      setErrors(dataErrors);
+    };
+    validateErrors();
+  }, [data]);
 
-    const [data, setData] = useState(initialValues);
-    const [errors, setErrors] = useState(initialErrors);
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
 
-    useEffect(() => {
-		const validateErrors = () => {
-			const dataErrors = {
-				emailError: (data.email?'':"Email is required") || (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(data.email) ? "":"Invalid Email"),
-			}
-			setErrors(dataErrors);
-		}
-		validateErrors()
-	}, [data]);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-    const handleChange = (e) => {
-		setData({ ...data, [e.target.name]: e.target.value });
-		
-	};
+    const isValid = !Object.values(errors).some((v) => v);
+    setErrors({ ...errors, ["validate"]: true });
+    // console.log(isValid);
+    if (isValid) {
+      try {
+        // console.log("submit");
+        const response = await fetch("/api/lead", {
+          method: "POST",
+          body: JSON.stringify(data),
+        });
 
-    const handleSubmit = async(event) => {
-		event.preventDefault();
-
-		const isValid = !Object.values(errors).some(v => v);
-		setErrors({ ...errors, ['validate']: true })
-        console.log(isValid);
-        if(isValid ){
-         
-            try {
-                console.log('submit')
-                const response = await fetch("/api/lead", {
-                method: "POST",
-                body: JSON.stringify(data),
-                });
-
-              
-                
-                if (response.ok) {
-                const res = await response.json()
-                console.log(res.lead.success);
-                if(res.lead.success == "success"){
-                   
-                    console.log('done....')
-                    alert('Email successfully sent! We will get in touch with you soon...');
-                }else{
-                    alert('Email Already Exists')
-                }
-                
-                }else{
-                    alert('An error occurred')
-                }
-            } catch (error) {
-                console.log(error);
-            } finally {
-                //set
-            }
-		}else{
-            alert(errors.emailError);
+        if (response.ok) {
+          const res = await response.json();
+          // console.log(res.lead.success);
+          if (res.lead.success == "success") {
+            // console.log("done....");
+            alert("Email successfully sent! We will get in touch with you soon...");
+          } else {
+            alert("Email Already Exists");
+          }
+        } else {
+          alert("An error occurred");
         }
-	}
+      } catch (error) {
+        console.log(error);
+      } finally {
+        //set
+      }
+    } else {
+      alert(errors.emailError);
+    }
+  };
 
-
-
-    return (
-        <section className="tw-py-12 tw-bg-gray-100/50">
-            <div className="container">
-            <div className="row tw-justify-center">
-                <div className="col-xl-9">
-                <div className="row tw-justify-center tw-items-center">
-                    <div className="col-xl-4">
-                    <div className="tw-w-1/2 tw-m-auto xl:tw-w-full">
-                        <Image 
-                        src="https://cdn.vnoc.com/icons/envelope1.png"
-                        height={500}
-                        width={500}
-                        alt=""
-                        className="tw-w-full img-fluid"
-                        />
-                    </div>
-                    </div>
-                    <div className="col-xl-8 tw-flex tw-flex-col">
-                    <h2 className="tw-font-bold tw-text-3xl tw-text-[#020E1E] mb-3">
-                    Learn more about us
-                    </h2>
-                    <p className="tw-text-gray-400">
-                    Join the Network of Professionals and Digital Asset Owners.
-                    </p>
-                    <div className="row">
-                        <div className="col-lg-9 tw-mb-3 sm:tw-mb-auto">
-                        <input type="text" name="email" onChange={handleChange} className="form-control form-control-lg" placeholder="Email Address" />
-                        </div>
-                        <div className="col-lg-3">
-                        <div className="d-grid">
-                            <button  onClick={handleSubmit} className="btn btn-primary btn-lg px-4">
-                            Submit
-                            </button>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
+  return (
+    <section className="tw-py-12 tw-bg-gray-100/50">
+      <div className="container">
+        <div className="row tw-justify-center">
+          <div className="col-xl-9">
+            <div className="row tw-justify-center tw-items-center">
+              <div className="col-xl-4">
+                <div className="tw-w-1/2 tw-m-auto xl:tw-w-full">
+                  <Image
+                    src="https://cdn.vnoc.com/icons/envelope1.png"
+                    height={500}
+                    width={500}
+                    alt=""
+                    className="tw-w-full img-fluid"
+                  />
                 </div>
+              </div>
+              <div className="col-xl-8 tw-flex tw-flex-col">
+                <h2 className="tw-font-bold tw-text-3xl tw-text-[#020E1E] mb-3">
+                  Learn more about us
+                </h2>
+                <p className="tw-text-gray-400">
+                  Join the Network of Professionals and Digital Asset Owners.
+                </p>
+                <div className="row">
+                  <div className="col-lg-9 tw-mb-3 sm:tw-mb-auto">
+                    <input
+                      type="text"
+                      name="email"
+                      onChange={handleChange}
+                      className="form-control form-control-lg"
+                      placeholder="Email Address"
+                    />
+                  </div>
+                  <div className="col-lg-3">
+                    <div className="d-grid">
+                      <button
+                        onClick={handleSubmit}
+                        className="btn btn-primary btn-lg px-4"
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  </div>
                 </div>
+              </div>
             </div>
-            </div>
-        </section>
-    )
-}
-export default Learn
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Learn;
--- lib/data.jsx
+++ lib/data.jsx
@@ -5,15 +5,13 @@ let DOMAIN = process.env.NEXT_PUBLIC_VERCEL_URL;
 export function getDomain() {
   const headersList = headers();
   const referrer = headersList.get("host");
-  const domainName = referrer.includes("localhost")
-    ? DOMAIN
-    : referrer.replace("www.", "");
+  const domainName = referrer.includes("localhost") ? DOMAIN : referrer.replace("www.", "");
   return domainName;
 }
 
 export async function getData() {
   const domain = getDomain();
-  const url = process.env.CONTRIB_API1+`&domain=${domain}`
+  const url = process.env.CONTRIB_API1 + `&domain=${domain}`;
   const res = await fetch(url, { next: { revalidate: 3600 } });
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
@@ -24,51 +22,50 @@ export async function getData() {
 }
 
 export async function getCategories() {
-    const domain = getDomain();
-    const url = process.env.GET_CATEGORIES+`&domain=${domain}`
-    const res = await fetch(url, { next: { revalidate: 3600 } });
-    if (!res.ok){
-        // This will activate the closest `error.js` Error Boundary
-        throw new Error("Failed to fetch data");
-    }
+  const domain = getDomain();
+  const url = process.env.GET_CATEGORIES + `&domain=${domain}`;
+  const res = await fetch(url, { next: { revalidate: 3600 } });
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
 
-    return res.json();
+  return res.json();
 }
 
 export async function getchatdomains(limit, keyword, sort) {
-    const domain = getDomain();
-    const url = process.env.GET_CHAT_DOMAINS+`&domain=${domain}&limit=${limit}&keyword=${keyword}&sort=${sort}`
-    const res = await fetch(url, { next: { revalidate: 3600 } });
-    if (!res.ok){
-        // This will activate the closest `error.js` Error Boundary
-        throw new Error("Failed to fetch data");
-    }
+  const domain = getDomain();
+  const url =
+    process.env.GET_CHAT_DOMAINS +
+    `&domain=${domain}&limit=${limit}&keyword=${keyword}&sort=${sort}`;
+  const res = await fetch(url, { next: { revalidate: 3600 } });
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
 
-    return res.json();
+  return res.json();
 }
-
 
 export async function getScript(url) {
   try {
     const res = await axios.get(url);
     return res.data.data.content;
   } catch (e) {
-    console.log("error getScript", e);
+    // console.log("error getScript", e);
     return { error: "error getScript" };
   }
 }
 
 export async function getBlogs() {
   const domain = getDomain();
-  const url = process.env.GET_BLOGS+`&domain=${domain}`
+  const url = process.env.GET_BLOGS + `&domain=${domain}`;
   const res = await fetch(url, { next: { revalidate: 3600 } });
- 
-  
-  if (!res.ok){
-      // This will activate the closest `error.js` Error Boundary
-      throw new Error("Failed to fetch data");
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
   }
 
   return res.json();
 }
-
